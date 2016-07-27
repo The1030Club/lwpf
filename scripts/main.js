6bdaf57b0fd82fe53a688b6da3705bb79b157326
Ben Haaland <bphaaland@gmail.com>
2016-07-27 09:15:43 -0400

    adding files

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+	// slideout.js
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  // tab toggle
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
